c8c5f3d8c3c026739397bfc18993b1967910f5ba
EllisJLC <jia.liang.chen@ryerson.ca>
2022-11-14 19:48:30 -0500

    thing

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -28,8 +28,8 @@ export const getDb = async () => {
   const textDb = await openDB('jate', 1); // get database
   const tx = textDb.transaction('jate', 'readonly'); // set text data to read only
   const store = tx.objectStore('jate'); // set store to object store of text
-  const request = store.get(1); // get all data from the store
-  const result = await request;
+  const request = await store.get(1); // get all data from the store
+  const result = request;
   console.log("Text retrieved!")
   console.log(result)
   return result.value;
